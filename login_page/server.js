ef98b3f75812add3308134dbd50ece8ed76ad44e
bread <binlepain178@kaist.ac.kr>
2019-05-19 08:46:17 +0000

    5/19 17:41

--- login_page/server.js
+++ login_page/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 db.once('open', () => console.log('connected to mongodb'));
-mongoose.connect('mongodb://localhost/SPARCS_NewbieProject/login_page', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/login_page', { useNewUrlParser: true });
 
 app.use(express.static('static/views'));
 app.set('views', __dirname + '/static/views');
+app.engine('html', ejs.renderFile);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.renderFile(__dirname + '/static/views/main.html');
-})
+    res.sendFile(__dirname + '/static/views/main.html');
+});
 
+const server = app.listen(80, () => {
+	console.log('server running at port 80');
+});
