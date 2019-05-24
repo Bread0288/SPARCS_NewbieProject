dfc2f0467e3d0536f43f8672b0bc05cac19e7968
Jinho Park <binlepain178@kaist.ac.kr>
2019-05-24 20:24:33 +0900

    5/24 20:23
    there is no json file in req

--- login_page/api/api.js
+++ login_page/api/api.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 router.post('/sign_in', (req, res) => {
     const user = new User();
-    console.log('id : ' + req.body.id + ' pw : ' + req.body.password);
+    const id = document.getElementById('id').value;
+    const pw = document.getElementById('password').value;
     user.id = req.body.id;
     user.pw = req.body.password;
+    console.log('id : ' + user.id + ' pw : ' + user.pw);
     user.save((err, result) => {
         if (err) return res.status(500).end('DB error');
         //return res.sendStatus(200);
