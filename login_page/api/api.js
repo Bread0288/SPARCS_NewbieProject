b555b3cb69d4462f25b02ab282094382107615e0
bread <binlepain178@kaist.ac.kr>
2019-05-24 12:15:02 +0000

    Save in DB succesively

--- login_page/api/api.js
+++ login_page/api/api.js
@@ -4,11 +4,10 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/sign_in', (req, res) => {
-    const user = new User();
-    const id = document.getElementById('id').value;
-    const pw = document.getElementById('password').value;
-    user.id = req.body.id;
-    user.pw = req.body.password;
+    console.log(req.body);
+	 const user = new User();
+	 user.id = req.body.id;
+	 user.pw = req.body.password;
     console.log('id : ' + user.id + ' pw : ' + user.pw);
     user.save((err, result) => {
         if (err) return res.status(500).end('DB error');
